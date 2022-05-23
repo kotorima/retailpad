30ea660bc7755bb33a635dc33324bb7ed937580b
kotorima <kotorimas@gmail.com>
2022-05-23 12:06:27 +0300

    save backup settings

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 
-import style from './header.scss';
+import { header } from './style.module.scss';
 
 function Header() {
+  console.log(header);
+
   return (
-    <div className={style.header}>
+    <div className={header}>
       <h1>Hello, World!</h1>
       <button>Жамк</button>
     </div>
