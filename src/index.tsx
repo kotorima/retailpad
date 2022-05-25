002c0d6f0e34d7fd8816273833746cb62cb1de2c
kotorima <kotorimas@gmail.com>
2022-05-25 11:58:00 +0300

    fixed problem in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import store from './store/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './store/store';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <
-    Provider store = { store } >
-    <
-    App / >
-    <
-    /Provider>
+
+root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
